b09fce9c553f94cb225cb02c712ed37e32b3d3cc
Corentin Desilles <corentindesilles974@gmail.com>
2023-04-17 12:58:56 +0200

    Content component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import Content from './components/Content/Content';
+import Content from './components/Content/Content';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import styles from './App.module.scss';
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className={`d-flex flex-column ${styles.appContainer}`}>
       <Header />
-      {/* <Content /> */}
+      <Content />
       <Footer />
     </div>
   );
