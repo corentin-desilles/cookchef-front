f08bf4e806ac091dad6d29acc032ae086fd5fcc0
Corentin Desilles <corentindesilles974@gmail.com>
2023-04-17 12:38:35 +0200

    front page base layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Content from './components/Content/Content';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import styles from './App.module.scss';
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
