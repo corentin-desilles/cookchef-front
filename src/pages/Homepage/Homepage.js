78e1996612d9e8e877d292ea2b69920d30a5bdff
Corentin Desilles <corentindesilles974@gmail.com>
2023-04-18 11:29:33 +0200

    paging

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -9,6 +9,7 @@ function Homepage() {
   const [recipes, setRecipes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [filter, setFilter] = useState('');
+  const [page, setPage] = useState(1);
   const BASE_URL_API = useContext(ApiContext);
 
   useEffect(() => {
@@ -16,10 +17,16 @@ function Homepage() {
     async function fetchRecipes() {
       try {
         setIsLoading(true);
-        const response = await fetch(BASE_URL_API);
+        const response = await fetch(
+          `${BASE_URL_API}?skip=${(page - 1) * 18}&limit=18`
+        );
         if (response.ok && !cancel) {
-          const recipes = await response.json();
-          setRecipes(Array.isArray(recipes) ? recipes : [recipes]);
+          const newRecipes = await response.json();
+          setRecipes(x =>
+            Array.isArray(newRecipes)
+              ? [...x, ...newRecipes]
+              : [...x, newRecipes]
+          );
         }
       } catch (e) {
         console.log('ERREUR');
@@ -31,7 +38,7 @@ function Homepage() {
     }
     fetchRecipes();
     return () => (cancel = true);
-  }, [BASE_URL_API]);
+  }, [BASE_URL_API, page]);
 
   function updateRecipe(updatedRecipe) {
     setRecipes(
@@ -41,7 +48,10 @@ function Homepage() {
 
   return (
     <div className="flex-fill container p-20">
-      <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+      <h1 className="my-30">
+        Découvrez nos nouvelles recettes{' '}
+        <small className={styles.small}>- {recipes.length}</small>
+      </h1>
       <div className={`card p-20 ${styles.contentCard}`}>
         <SearchBar setFilter={setFilter} />
         {isLoading && !recipes.length ? (
@@ -59,6 +69,11 @@ function Homepage() {
               ))}
           </div>
         )}
+        <div className="d-flex flex-row justify-content-center align-items-center p-20">
+          <button onClick={() => setPage(page + 1)} className="btn btn-primary">
+            Charger plus de recettes
+          </button>
+        </div>
       </div>
     </div>
   );
