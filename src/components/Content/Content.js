34512b08942c43d9565af59a228a20b2f779fb4a
Corentin Desilles <corentindesilles974@gmail.com>
2023-04-18 10:12:06 +0200

    SearchBar component

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,18 +1,24 @@
 import styles from './Content.module.scss';
 import Recipe from '../Recipe/Recipe';
 import { data } from '../../data/recipes';
+import { useState } from 'react';
+import SearchBar from './SearchBar/SearchBar';
 
 function Content() {
   const recipes = data;
+  const [filter, setFilter] = useState('');
 
   return (
     <div className="flex-fill container p-20">
       <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
       <div className={`card p-20 ${styles.contentCard}`}>
+        <SearchBar setFilter={setFilter} />
         <div className={styles.grid}>
-          {recipes.map(r => (
-            <Recipe key={r._id} title={r.title} image={r.image} />
-          ))}
+          {recipes
+            .filter(r => r.title.toLowerCase().startsWith(filter))
+            .map(r => (
+              <Recipe key={r._id} title={r.title} image={r.image} />
+            ))}
         </div>
       </div>
     </div>
