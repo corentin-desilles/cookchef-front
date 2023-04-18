9cdc3f29823a626bd0f04ef4d20871ff08629b40
Corentin Desilles <corentindesilles974@gmail.com>
2023-04-18 10:56:26 +0200

    simulate fetch data from API with UseContext

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './style/index.scss';
 import App from './App';
+import { ApiContext } from './context/ApiContext';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <ApiContext.Provider value="https://restapi.fr/api/recipes">
+      <App />
+    </ApiContext.Provider>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,28 +1,57 @@
 import styles from './Homepage.module.scss';
 import Recipe from './components/Recipe/Recipe';
-import { data } from '../../data/recipes';
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import SearchBar from './components/SearchBar/SearchBar';
+import Loading from '../../components/Loading/Loading';
+import { ApiContext } from '../../context/ApiContext';
 
-function Content() {
-  const recipes = data;
+function Homepage() {
+  const [recipes, setRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [filter, setFilter] = useState('');
+  const BASE_URL_API = useContext(ApiContext);
+
+  useEffect(() => {
+    let cancel = false;
+    async function fetchRecipes() {
+      try {
+        setIsLoading(true);
+        const response = await fetch(BASE_URL_API);
+        if (response.ok && !cancel) {
+          const recipes = await response.json();
+          setRecipes(Array.isArray(recipes) ? recipes : [recipes]);
+        }
+      } catch (e) {
+        console.log('ERREUR');
+      } finally {
+        if (!cancel) {
+          setIsLoading(false);
+        }
+      }
+    }
+    fetchRecipes();
+    return () => (cancel = true);
+  });
 
   return (
     <div className="flex-fill container p-20">
       <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
       <div className={`card p-20 ${styles.contentCard}`}>
         <SearchBar setFilter={setFilter} />
-        <div className={styles.grid}>
-          {recipes
-            .filter(r => r.title.toLowerCase().startsWith(filter))
-            .map(r => (
-              <Recipe key={r._id} title={r.title} image={r.image} />
-            ))}
-        </div>
+        {isLoading && !recipes.length ? (
+          <Loading />
+        ) : (
+          <div className={styles.grid}>
+            {recipes
+              .filter(r => r.title.toLowerCase().startsWith(filter))
+              .map(r => (
+                <Recipe key={r._id} title={r.title} image={r.image} />
+              ))}
+          </div>
+        )}
       </div>
     </div>
   );
 }
 
-export default Content;
+export default Homepage;
